d68289dd1c79438b7695c611bfb027bf3c87aab3
Stefano Primo <stefano.primo@eos-solutions.it>
2020-09-03 11:00:41 +0200

    New scripts

---
+++ Docker/Copy Event Log.ps1
@@ -0,0 +1,3 @@
+# creates a backup of application event log and save it into shared folder (C:\Run\My)
+
+wevtutil epl Application C:\run\my\AppLogBackup.evtx
---
+++ SQL/Delete User session and breakpoints.sql
@@ -0,0 +1,14 @@
+delete from [Access Control]
+delete from [Active Session]
+delete from [User]
+delete from [User Default Style Sheet]
+delete from [User Group Member]
+delete from [User Metadata]
+delete from [User Personalization]
+delete from [User Property]
+
+delete from [Debugger Breakpoint]
+
+delete from [Server Instance]
+truncate table [Session Event]
+delete from [Active Session]

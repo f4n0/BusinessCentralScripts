11d90918abd641b6f662bf4191c8cd74a8c695fc
Stefano Primo <stefano.primo@eos-solutions.it>
2020-12-21 11:00:02 +0100

    add new docker command

---
+++ Docker/Generate Symbols in Nav Container.ps1
@@ -0,0 +1,4 @@
+﻿$containerName = 'BC14-CU11-IT'
+$credential = New-Object pscredential 'admin', (ConvertTo-SecureString -String 'Password123!' -AsPlainText -Force)
+
+Generate-SymbolsInNavContainer -containerName $containerName -sqlCredential $credential

cc3d306a2ff488e993498ae0a99dafdc6f1569b5
Stefano Primo <stefano.primo@eos-solutions.it>
2020-12-29 09:44:40 +0100

    Add custom Addins

---
+++ Docker/Copy custom Addins to container.ps1
@@ -0,0 +1,27 @@
+﻿$containerName = 'NP-BC14'
+$AddinLocation = ''#path to addin location
+
+$RunMy = ((Get-BcContainerSharedFolders -containerName $containerName).GetEnumerator() | Where-Object { $_.Value -eq "c:\run\my" }).Key
+
+New-Item -ItemType Directory -Path $RunMy -Name "customAddin" -ErrorAction Ignore
+
+
+Copy-Item -Recurse "$AddinLocation\*" "$RunMy\customAddin"
+
+$ContainerId = Get-NavContainerId -containerName $containerName
+Write-Host $ContainerId
+$Session = New-PSSession -ContainerId $ContainerId -RunAsAdministrator
+
+try {
+
+    Invoke-Command -Session $Session -ScriptBlock {
+         $serviceTierFolder = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service").FullName
+        Copy-Item -Recurse "C:\Run\my\customAddin" (Join-Path $serviceTierFolder "Add-ins") 		
+		
+    }
+}
+finally {
+    if ($null -ne $Session) {
+        Remove-PSSession $Session
+    }
+}
